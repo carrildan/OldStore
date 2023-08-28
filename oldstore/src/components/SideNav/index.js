f465da085c7aaf55d1669aa55c7161af0d773766
Daniel <car15114@byui.edu>
2023-08-28 04:41:53 +0200

    started implementing SideNav component

---
+++ oldstore/src/components/SideNav/index.js
@@ -0,0 +1,12 @@
+import "./_side-nav.scss";
+
+const SideNav = () =>{
+    return(
+        <div className="side-nav">
+            <div className="section-title">
+                <h3>Category</h3>
+            </div>
+        </div>
+    )
+}
+export default SideNav;
