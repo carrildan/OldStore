bd8286b3f544073cb5677a6cbcf0636e2de29e5e
Daniel <car15114@byui.edu>
2023-08-28 04:07:33 +0200

    created CatNav.js component with 4 categories

--- oldstore/src/App.js
+++ oldstore/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TopNav from './components/TopNav/TopNav';
+import CatNav from './components/CatNav/CatNav';
 
 function App() {
   return (
     <div className="App">
       {/* ------------------------------------------- HEADER ------------------------------------------- */}
       <TopNav/>
+      <CatNav/>
       {/* ------------------------------------------- HEADER END ------------------------------------------- */}
       
     </div>
