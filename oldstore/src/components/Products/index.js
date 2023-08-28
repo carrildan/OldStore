c0904c3494dad298cac32ed87d79ae8ce71ecdd5
Daniel <car15114@byui.edu>
2023-08-28 19:13:48 +0200

    started implementing Products component

---
+++ oldstore/src/components/Products/index.js
@@ -0,0 +1,20 @@
+import "./_products.scss";
+
+const Products = () =>{
+    return(
+        <div>
+            <div className="mx-5 p-3 col-lg-3 col-md-6 product-card">
+                <div className="product-image-container">
+                    <img src={require("../../assets/images/ProductImages/shop-1.jpg")} />
+                </div>
+
+                <div className="product-info">
+                    <h5><a href="#">Jacket</a></h5>
+                    <p className="product-price"> $45 </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Products;
