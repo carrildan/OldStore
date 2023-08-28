81b893f729b21e77b0edd9966624c94163874335
Daniel <car15114@byui.edu>
2023-08-28 02:25:25 +0200

    removed logo import | started implementing login, cart and whishlist

--- oldstore/src/App.js
+++ oldstore/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -24,6 +23,17 @@ function App() {
             <button> <i className="fa fa-search"/> </button>
           </div>
           
+          <div className="login-container p-0">
+            <i className="fa fa-user-circle user-icon"/>
+            <h5><a href="#"> Login </a></h5>
+          </div>
+
+          <div className="cart-wishlist">
+            <ul className="p-0">
+              <li className="list-icon"> <i className="fa fa-heart"/> </li>
+              <li className="list-icon"> <i className="fa fa-shopping-cart"/> </li>
+            </ul>
+          </div>
         </div>
       </div>
       {/* ------------------------------------------- HEADER END ------------------------------------------- */}
