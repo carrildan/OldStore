95b0812805d17dad1212d173502269a8e041d8d4
Daniel <car15114@byui.edu>
2023-09-04 19:59:29 +0200

    removed store from index.js to store folder and created a file for the store

--- oldstore/src/index.js
+++ oldstore/src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { configureStore } from "@reduxjs/toolkit";
-
-const store = configureStore({
-  reducer:{
-
-  }
-})
+import { store } from './Redux/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
