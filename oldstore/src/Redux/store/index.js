9133b740e21c0cb16eff7f33c9b4dd48121626b5
Daniel <car15114@byui.edu>
2023-09-05 15:43:23 +0200

    added productSlice to the store

--- oldstore/src/Redux/store/index.js
+++ oldstore/src/Redux/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CategorySlice from "../Category/categorySlice";
+import productSlice from "../Product/productSlice";
 
 export const store = configureStore({
     reducer:{
-        categoryReducer : CategorySlice
+        categoryReducer : CategorySlice,
+        pr: productSlice
     }
 })
