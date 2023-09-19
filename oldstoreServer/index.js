1595e0cafb1856ffb5c577c9aa49331dc6a28ab6
Daniel <car15114@byui.edu>
2023-09-19 18:31:53 +0200

    changed require() variable´s name to product

--- oldstoreServer/index.js
+++ oldstoreServer/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const productCategories = require("./Routes/ProductCategories");
+const product = require("./Routes/ProductCategories");
 const app = express();
 const cors = require("cors");
 
 app.use(cors());
 
-app.use("/",productCategories);
+app.use("/",product);
 
 const PORT = 5001;
 
 const server = app.listen(PORT, ()=>{
-    console.log(`App is running on port ${PORT}`);
+    console.log(`App is running on port 5001`);
 });
